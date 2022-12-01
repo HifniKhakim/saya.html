ed9f30b70553932b9cb509db780599f049a951e5
HifniKhakim <112689567+HifniKhakim@users.noreply.github.com>
2022-12-01 09:46:07 +0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,12 @@
+function myFunction(aku) {
+    document.getElementById("aku").innerHTML = "aku itu tidak penting";
+  }
+  function myFunction(kamu) {
+    document.getElementById("kamu").innerHTML = "aku bukan siapa siapa";
+  }
+  function myFunction(dia) {
+    document.getElementById("dia").innerHTML = "jangan tanya saya";
+  }
+  function myFunction(saya) {
+    document.getElementById("demo").innerHTML = "cahaya telah menerangimu";
+  }
